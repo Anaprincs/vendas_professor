63388a4865bfcb2a9eeb913070e464ae0f199e7a
Anaprincs <hipolitocarolinaana@gmail.com>
2025-04-08 13:14:04 -0300

    eu editei o funcionarios e dashbboards

--- app/Livewire/Admin/Funcionario/FuncionarioIndex.php
+++ app/Livewire/Admin/Funcionario/FuncionarioIndex.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire\Admin\Funcionario;
 
+use App\Models\Funcionario;
 use Livewire\Component;
 
 class FuncionarioIndex extends Component
 {
     public function render()
     {
-        return view('livewire.admin.funcionario.funcionario-index');
+
+        $funcionarios = Funcionario::all();
+
+        return view('livewire.admin.funcionario.funcionario-index',
+        compact('funcionarios')); // -> pega os dados da variavel e envia pra view
+
     }
 }
